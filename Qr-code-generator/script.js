f08bb6bc30530d477bfdd0fff4157c81eb89148c
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-03-03 15:29:18 +0530

    successfully created qr code generator button

---
+++ Qr-code-generator/script.js
@@ -0,0 +1,39 @@
+const qrContainer=document.querySelector('.qr-container');
+
+const qrTextInput=document.querySelector('.qr-text')
+
+const generateQrCodebtn=document.querySelector('.generate-qr-code-btn');
+
+console.log(qrContainer,qrTextInput,generateQrCodebtn);
+
+generateQrCodebtn.addEventListener("click",()=>{
+
+    validateInputField();
+})
+
+function validateInputField(){
+    
+    if(qrTextInput.value .trim().length>0){
+        generateQrCode();
+    }
+    else{
+        errorMessageText.textContent="Enter text and use some Url to ge4nerate code";
+    }
+}
+
+const content=document.querySelector(".content");
+
+console.log(content);
+
+function generateQrCode(){
+    qrContainer.innerHTML="";
+    new QRCode (qrContainer,{
+        text:qrTextInput.value,
+        height:400,
+        width:400,
+        colorLight:'#fff',
+        colorDark:'#000'
+    });
+    qrTextInput.value="";
+    errorMessageText.textContentl="";
+}
